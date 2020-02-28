19033691d057a8a94bf05272bab459a2d88c3591
Michael Pyne <michael.pyne@gmail.com>
2020-02-27 22:55:03 -0500

    The sample user router was removed already.

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -21,7 +20,6 @@ app.use('/uswds', express.static(path.join(__dirname, 'node_modules/uswds/dist')
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
